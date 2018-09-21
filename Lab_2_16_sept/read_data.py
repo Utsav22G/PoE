3b618f4673bd4a6ed611766c70c399ff473500ab
aidenclpt <aiden.carley-clopton@students.olin.edu>
2018-09-21 11:43:02 -0400

    updating read_data

--- Lab_2_16_sept/read_data.py
+++ Lab_2_16_sept/read_data.py
@@ -1,4 +1,5 @@
 import math as m
+import numpy as np
 from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
 import ast
@@ -12,19 +13,25 @@ theta = ast.literal_eval(file.readline())
 
 
 for i in range(len(theta)):
-    theta[i] = theta[i]*2*m.pi/180
-    psi[i] = psi[i]*2*m.pi/180
+    if r[i] > 30*2.54:
+        r[i] = -5
+
+    # theta[i] = -(93 - theta[i])*m.pi/180
+    # psi[i] = (93 - psi[i])*m.pi/180
+    theta[i] = np.radians(theta[i])
+    psi[i] = np.radians(psi[i])
 
 x = []
 y = []
 z = []
 
 for i in range(len(r)):
-    x.append(r[i]*m.sin(psi[i])*m.cos(theta[i]))
-    y.append(r[i]*m.sin(psi[i])*m.sin(theta[i]))
-    z.append(r[i]*m.cos(psi[i]))
+    x.append(r[i]*m.sin(theta[i])*m.cos(psi[i]))
+    y.append(r[i]*m.sin(theta[i])*m.sin(psi[i]))
+    z.append(r[i]*m.cos(theta[i]))
 
 fig = plt.figure()
 ax = plt.axes(projection = '3d')
 
 ax.scatter3D(x, y, z, 'gray')
+plt.show()
