0618c1d4f519f6696ac14d510a2d793618f3e578
Utsav Gupta <utsav22g@gmail.com>
2018-09-28 15:00:51 -0400

    Adding code for robot

--- Lab_3/Lab_3.ino
+++ Lab_3/Lab_3.ino
@@ -3,18 +3,36 @@
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
+// Defining pins
+#define LEFT_SENSOR A0
+#define RIGHT_SENSOR A1
 
+
+// Creating motor object
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
 // getMotor(port#)  -->   look at the motor shield
-Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
+Adafruit_DCMotor *left_motor = AFMS.getMotor(4);
+Adafruit_DCMotor *right_motor = AFMS.getMotor(3);
 
 void setup() {
+  analogReference(DEFAULT);
+
+  Serial.begin(115200);
   AFMS.begin();
-  myMotor->setSpeed(150);
+
 
 }
 
 void loop() {
+  int left_ir = analogRead(LEFT_SENSOR);
+  int right_ir = analogRead(RIGHT_SENSOR);
+
+  left_motor -> setSpeed((left_ir/right_ir)*500);
+  right_motor -> setSpeed((right_ir/left_ir)*500);
+
+  left_motor -> run(FORWARD);
+  right_motor -> run(FORWARD);
+
 
 }
